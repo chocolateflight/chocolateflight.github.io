acf3a02630e5b0280a97a49b6c4affc123708f5d
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-04-07 21:44:43 +0200

    Menu now closes when selecting menu item

--- resources/script/script.js
+++ resources/script/script.js
@@ -2,6 +2,7 @@
 
 let menuTarget = document.getElementById("main-menu");
 let sideMenu = document.getElementById("sidemenu");
+let sideMenuItem = document.getElementsByClassName("side-menu-item");
 let isMenuOpen = 0;
 
 sideMenu.style.display = "none"
@@ -17,7 +18,13 @@ let openMenu = function() {
 };
 
 let closeMenu = function() {
-    sideMenu.hidden = true;
+    sideMenu.style.display = "none";
+    isMenuOpen = 0;
 }
 
 menuTarget.addEventListener("click", openMenu);
+
+for (let i = 0; i < sideMenuItem.length; i++) {
+    sideMenuItem[i].addEventListener("click", closeMenu);
+}
+
